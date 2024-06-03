bac4fe6af92603eb0d5c22fed3f1c15a8d7c51f2
jet2tlf <jet2tlf@gmail.com>
2024-06-03 13:57:16 -0300

    codecrafters submit [skip ci]

--- app/handler.go
+++ app/handler.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+var directory = ""
+
+func init() {
+	args := os.Args
+	index := slices.Index(args, "--directory")
+	if index > 0 {
+		directory = args[index+1]
+	}
+
+	fmt.Println("directory: ", directory)
+}
 
 func basePathHandler(request *Request) *Response {
 	return NewResponse(Ok, nil, nil)
@@ -26,8 +43,25 @@ func userAgentHandler(request *Request) *Response {
 	}, []byte(header))
 }
 
+func fileHandler(request *Request) *Response {
+	file, err := os.Open(filepath.Join(directory, strings.Split(request.Target(), "/")[2]))
+	if err != nil {
+		return NewResponse(NotFound, nil, nil)
+	}
+
+	buf, _ := io.ReadAll(file)
+	return NewResponse(Ok, map[string]string{
+		"Content-Type":   "application/octet-stream",
+		"Content-Length": strconv.Itoa(len(buf)),
+	}, buf)
+}
+
 func Register(builder *RouterBuilder) {
 	builder.Add("GET", regexp.MustCompile("^/$"), basePathHandler)
 	builder.Add("GET", regexp.MustCompile("^/echo/[a-zA-Z]+$"), echoHandler)
 	builder.Add("GET", regexp.MustCompile("^/user-agent$"), userAgentHandler)
+
+	if directory != "" {
+		builder.Add("GET", regexp.MustCompile("^/files/.+$"), fileHandler)
+	}
 }
